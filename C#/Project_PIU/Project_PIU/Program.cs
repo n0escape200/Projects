846e8c54efcae0772b4b4993bef5e6b0ae02d9a5
n0escape200 <n0_escape200@yahoo.com>
2024-03-19 21:35:11 +0200

    Added the first C# project

---
+++ C#/Project_PIU/Project_PIU/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Car;
+using Truck;
+using Motorcycle;
+using Repository;
+
+namespace Project_PIU
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Base class values
+            string brand, model, year, condition;
+            int km, price;
+
+            //Car values
+            int nrDoors, cp;
+            string body;
+
+            //Truck values
+            int cargo, tow;
+
+            //Motorcycle values 
+            int cc;
+            string type;
+
+            Console.WriteLine("Brand:");
+            brand = Console.ReadLine();
+
+            Console.WriteLine("Model:");
+            model = Console.ReadLine();
+
+            Console.WriteLine("Year:");
+            year = Console.ReadLine();
+
+            Console.WriteLine("Condition:");
+            condition = Console.ReadLine();
+
+            Console.WriteLine("Km:");
+            km = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Price:");
+            price = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Type:");
+            type = Console.ReadLine();
+
+            Console.WriteLine("Number of doors:");
+            nrDoors = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Horse power:");
+            cp = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Body chassis:");
+            body = Console.ReadLine();
+
+            Console.ReadKey(); // return 0;
+        }
+    }
+}
---
+++ C#/Project_PIU/Repository/RepositoryClass.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using Vehicle;
+
+namespace Repository
+{
+    internal class RepositoryClass:VehicleClass
+    {
+        List<VehicleClass> repository;
+
+        public RepositoryClass()
+        {
+            repository = new List<VehicleClass>();
+        }
+
+        public void Add(VehicleClass v)
+        {
+            repository.Add(v);
+        }
+
+        public void GetAll()
+        {
+            foreach (VehicleClass v in repository)
+            {
+                v.Info();
+            }
+        }
+
+        public void GetByIndex(int index)
+        {
+            repository[index].Info();
+        }
+
+        public void UpdateByIndex(int index, VehicleClass v)
+        {
+            repository[index] = v;
+        }
+
+        public void DeleteByIndex(int index)
+        {
+            repository.RemoveAt(index);
+        }
+    }
+}
